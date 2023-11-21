af9dae16d4d172ecb3b7d80b9a442a678d926fbf
mykola.zhuravlyov <gaaved@gmail.com>
2023-11-21 17:55:43 +0200

    develop fix bug and change SetState

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,22 +1,28 @@
 import {useState} from "react";
 
+
 function Calculator() {
-    const [numbers] = [Array(10).fill(0).map((_,i) => i )];
+    const numbers = Array(10).fill(0).map((_,i) => i );
     const signs = ['+', '-', '*', '/', '.', 'C', '<-'];
     const [stringCalculate, setStringCalculate] = useState('');
 
     function handleClick(props){
-        if(signs.includes(props) && !signs.includes(stringCalculate[stringCalculate.length -1]) && stringCalculate){
-            setStringCalculate(stringCalculate + props)
+        if(signs.includes(props) && !signs.includes(stringCalculate[stringCalculate.length -1]) && stringCalculate && props !== '<-'){
+            //setStringCalculate(stringCalculate + props)
+            setStringCalculate((stringCalculate) => stringCalculate + props)
         }
-        if(!signs.includes(props)){
-            setStringCalculate(stringCalculate + Number(props))
+        if(!signs.includes(props) && typeof stringCalculate !== 'number'){
+            //setStringCalculate(stringCalculate + props)
+            setStringCalculate((stringCalculate) => stringCalculate + props)
         }
         if(props === 'C' ){
             setStringCalculate('')
         }
         if(props === '<-' ){
-            setStringCalculate(stringCalculate.slice(0, -1))
+            //setStringCalculate(stringCalculate.slice(0, -1))
+            if(typeof stringCalculate === 'string'){
+                setStringCalculate((stringCalculate) => stringCalculate.slice(0, -1))
+            }
         }
     }
 
@@ -26,15 +32,15 @@ function Calculator() {
     }
 
     const listNumbers = numbers.reverse().map((number) =>
-        <button className="squareButton" onClick={() => handleClick(number)}>{number}</button>
+        <button key={number} className="squareButton" onClick={() => handleClick(String(number))}>{number}</button>
     );
     const listSigns = signs.map((sign) =>
-        <button className="squareButtonSigns" onClick={() => handleClick(sign)}>{sign}</button>
+        <button key={sign} className="squareButtonSigns" onClick={() => handleClick(sign)}>{sign}</button>
     );
 
     return (
-        <>
-            <input className="calculateString" type="string" value={stringCalculate}/>
+        <div>
+            <input className="calculateString" type="string" value={stringCalculate} readOnly={true}/>
             <div className="buttons">
                 <div className="calculateNumber">
                     {listNumbers}
@@ -44,7 +50,7 @@ function Calculator() {
                     <button className="squareButtonEquals" onClick={() => handleClickEquals(stringCalculate)}>=</button>
                 </div>
             </div>
-        </>
+        </div>
     );
 
 }
